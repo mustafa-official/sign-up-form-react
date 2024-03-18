0aaf5efb7a145ebf3abd70f0760b441c842dffbd
webdev-mostofa <mdmostofaa24@gmail.com>
2024-03-18 06:31:16 +0600

    project ready

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Form from "./components/Form/Form";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center">
+        <Form></Form>
+      </div>
+    </>
+  );
+}
+
+export default App;
